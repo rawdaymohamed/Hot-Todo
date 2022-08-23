f1cd9046d14239c755f8c44fcb167800e6e23c0c
Rawda Yasser <rawda.developer@gmail.com>
2022-08-23 02:07:30 +0200

    Feat: add setup for tailwindcss and daisyui for style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './todos/TodoList';
 
 function App() {
-  return <h2>Hot Todo</h2>;
+  return (
+    <>
+      <h2>Hot Todo</h2>
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
