6a342833846f65dda996b2d1ed4eafd1e5bbc793
Rawda Yasser <rawda.developer@gmail.com>
2022-08-24 06:05:58 +0200

    Feat: add redux persist library to the frontend to persist todos in the local storage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { configureStore } from './store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const store = configureStore();
+const persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={configureStore()}>
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
